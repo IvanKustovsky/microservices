ccc9b652a933bea65eb8d3447bc5b1dc65fa66e3
IvanKustovsky <vanyakustovsky@gmail.com>
2024-06-05 12:13:14 +0300

    add delete account API

--- section1/accounts/src/main/java/com/example/accounts/service/IAccountService.java
+++ section1/accounts/src/main/java/com/example/accounts/service/IAccountService.java
@@ -10,7 +10,24 @@ public interface IAccountService {
      */
     void createAccount(CustomerDto customerDto);
 
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Accounts details based on a given mobile number
+     */
     CustomerDto fetchAccount(String mobileNumber);
 
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     * @return boolean indicating if the update of Account is successful or not
+     */
     boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
 }

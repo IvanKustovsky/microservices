afc5e07f45075473b99ffafdad55d600804da133
IvanKustovsky <vanyakustovsky@gmail.com>
2024-06-10 11:59:58 +0300

    refresh configuration at runtime using actuator path (actuator/refresh)

--- section3/v2-spring-cloud-config/accounts/src/main/java/com/example/accounts/dto/AccountsContactInfoDto.java
+++ section3/v2-spring-cloud-config/accounts/src/main/java/com/example/accounts/dto/AccountsContactInfoDto.java
@@ -1,11 +1,17 @@
 package com.example.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts") // recommended approach
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails,
-                                     List<String> onCallSupport) {
+@ConfigurationProperties(prefix = "accounts")
+@Getter @Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }

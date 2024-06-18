93905a3c7f128fdfeeb18e7c7bc07042eca581ce
IvanKustovsky <vanyakustovsky@gmail.com>
2024-06-18 15:58:17 +0300

    implement RateLimiter pattern inside Accounts Microservice

--- section7/accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ section7/accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.example.accounts.dto.CustomerDto;
 import com.example.accounts.dto.ErrorResponseDto;
 import com.example.accounts.dto.ResponseDto;
 import com.example.accounts.service.IAccountService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -213,11 +214,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(summary = "Get Contact Info REST API",

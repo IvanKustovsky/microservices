56f23b7212709b83c476508b623f0ad2adb10d6f
IvanKustovsky <vanyakustovsky@gmail.com>
2024-06-18 12:04:00 +0300

    initial commit to the demo of implementing resilience in microservices

---
+++ section7/accounts/src/main/java/com/example/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.accounts.service.impl;
+
+import com.example.accounts.dto.*;
+import com.example.accounts.entity.Accounts;
+import com.example.accounts.entity.Customer;
+import com.example.accounts.exception.ResourceNotFoundException;
+import com.example.accounts.mapper.AccountMapper;
+import com.example.accounts.mapper.CustomerMapper;
+import com.example.accounts.repository.AccountRepository;
+import com.example.accounts.repository.CustomerRepository;
+import com.example.accounts.service.ICustomerService;
+import com.example.accounts.service.client.CardsFeignClient;
+import com.example.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomerService {
+
+    private final AccountRepository accountRepository;
+    private final CustomerRepository customerRepository;
+    private final CardsFeignClient cardsFeignClient;
+    private final LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+
+        Accounts accounts = accountRepository.findByCustomerId(customer.getCustomerId())
+                .orElseThrow(() -> new ResourceNotFoundException
+                        ("Account", "customerId", customer.getCustomerId().toString()));
+
+        var customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
+
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+    }
+}
